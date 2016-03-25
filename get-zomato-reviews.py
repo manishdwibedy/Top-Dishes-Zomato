2182e66958fd0e2290429091ca9857a62f20e151
Manish Dwibedy <manish.dwibedy@gmail.com>
2016-03-25 16:37:16 -0700

    Trying to hit the API, but not working currently.

--- get-zomato-reviews.py
+++ get-zomato-reviews.py
@@ -1,8 +1,24 @@
+import requests
 from util import constant
 
 def get_reviews(restraunt_id):
-    print constant.zomato_base_url
+    '''
+    Getting the reviews
+    :param restraunt_id: restraunt id
+    :return: Nothing
+    '''
+    URL = constant.zomato_base_url + 'reviews'
     print constant.zomato_api_key
+
+    headers = {
+        'Accept': 'application/json',
+        'user-key': constant.zomato_api_key
+    }
+
+    payload = {'res_id': restaurant_id}
+
+    response = requests.get(URL, headers=headers, params=payload)
+    print response.text
 
 if __name__ == "__main__":
     restaurant_id = constant.restaurant_id
