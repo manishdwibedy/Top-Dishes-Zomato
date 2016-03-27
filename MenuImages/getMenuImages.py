9188d746ca657bddbb6a8228b3ebaa42444e1fa0
Manish Dwibedy <manish.dwibedy@gmail.com>
2016-03-26 23:30:31 -0700

    Getting all the images in the page.

--- MenuImages/getMenuImages.py
+++ MenuImages/getMenuImages.py
@@ -1,12 +1,17 @@
 from bs4 import BeautifulSoup
 import urllib2
 
-URL = "https://www.zomato.com/ncr/impromptu-golf-course-road-gurgaon/menu#tabtop?utm_source=api_basic_user&utm_medium=api&utm_campaign=v2.1"
 
-page = BeautifulSoup(urllib2.urlopen(URL), "html.parser")
+def getMenuImage(URL):
+    page = BeautifulSoup(urllib2.urlopen(URL), "html.parser")
 
-for img in page.findAll('img'):
-    print img
+    for img in page.findAll('img'):
+        print img.attrs
+
+if __name__ == "__main__":
+    URL = "https://www.zomato.com/ncr/impromptu-golf-course-road-gurgaon/menu#tabtop?utm_source=api_basic_user&utm_medium=api&utm_campaign=v2.1"
+
+    getMenuImage(URL)
 
 
 
