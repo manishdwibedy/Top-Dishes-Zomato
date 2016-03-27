7bf6ae3ab1f785c5881b1841bb83a95db4e52a8b
Manish Dwibedy <manish.dwibedy@gmail.com>
2016-03-26 23:35:42 -0700

    Gettting the menu image from the page.

--- MenuImages/getMenuImages.py
+++ MenuImages/getMenuImages.py
@@ -6,7 +6,10 @@ def getMenuImage(URL):
     page = BeautifulSoup(urllib2.urlopen(URL), "html.parser")
 
     for img in page.findAll('img'):
-        print img.attrs
+        alt = img.attrs['alt']
+        if "Menu" in alt:
+            print img.attrs['src']
+            break
 
 if __name__ == "__main__":
     URL = "https://www.zomato.com/ncr/impromptu-golf-course-road-gurgaon/menu#tabtop?utm_source=api_basic_user&utm_medium=api&utm_campaign=v2.1"
