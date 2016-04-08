ebba33b01e473ffec789ca6e8a3f00ab493a15f7
Manish Dwibedy <manish.dwibedy@gmail.com>
2016-04-08 01:26:31 -0700

    Adding a web component to the application

--- web/run.py
+++ web/run.py
@@ -1,9 +1,16 @@
 from flask import Flask
+from flask import render_template
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    return 'Main Page'
+
+@app.route('/test')
+def annotator():
+    # return 'testing'
+    return render_template('annotations.html')
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host='0.0.0.0')
