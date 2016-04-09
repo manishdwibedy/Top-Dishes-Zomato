96a8ae5167d4cb751ec9f7bad91ed43baed81920
Manish Dwibedy <manish.dwibedy@gmail.com>
2016-04-09 15:51:05 -0700

    Can return the reviews as list of words in the JSON format.

--- web/run.py
+++ web/run.py
@@ -1,5 +1,5 @@
-from flask import Flask
-from flask import render_template
+from flask import Flask, render_template, jsonify
+import re
 
 app = Flask(__name__)
 
@@ -10,6 +10,13 @@ def hello_world():
 @app.route('/annotate')
 def annotator():
     return render_template('annotations.html')
+
+@app.route('/getReview')
+def getReviews():
+    review = 'Coffee was decent, but not memorable enough to become a regular.'
+    words = re.sub("[^\w]", " ",  review).split()
+
+    return jsonify(results=words)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
