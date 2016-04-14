6fb73a3bb28987b9b201ad4bdd9f6d032c162514
Manish Dwibedy <manish.dwibedy@gmail.com>
2016-04-14 09:57:51 -0700

    Now querying data needs one more parameter - collection_name

--- solr/query.py
+++ solr/query.py
@@ -1,14 +1,29 @@
 import connection
 
-def get(connection, query):
-    return connection.zomato.search({'q':query})
+def get(connection, collection, query):
+    """
+    Getting the data from collection matching the query
+    :param connection: the Solr Connection
+    :param collection: the Solr Collection
+    :param query: Solr Query
+    :return: the list of documents returned by Solr
+    """
 
-def getCount(connection, query):
-    docs = connection.zomato.search({'q':query, 'rows': 0})
+    return connection[collection].search({'q':query})
+
+def getCount(connection, collection, query):
+    """
+    Get only the number of documents from the collection matching the query
+    :param connection: the Solr Connection
+    :param collection: the Solr Collection
+    :param query: Solr Query
+    :return: the number of documents matched
+    """
+    docs = connection[collection].search({'q':query, 'rows': 0})
     return docs.result.response.numFound
 
 if __name__ == '__main__':
     solr = connection.get_connection()
-    print getCount(solr, '*:*')
+    print getCount(solr, 'zomato_reviews','*:*')
 
 
