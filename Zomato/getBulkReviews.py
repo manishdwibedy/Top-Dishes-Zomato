b7aaec2abc7741cd494868f0823ea8b0cf3bb68f
Manish Dwibedy <manish.dwibedy@gmail.com>
2016-04-14 09:32:54 -0700

    Added the docstring.

--- Zomato/getBulkReviews.py
+++ Zomato/getBulkReviews.py
@@ -3,6 +3,15 @@ import getReviews
 from util import constant
 
 def getAllReviews(city_id, start_city_index, count_city):
+    """
+    Getting the reviews for 'count_city' number of restaurants in the city referred by city_id.
+    The first restaurant's index would be start_city_index
+
+    :param city_id: the city's ID
+    :param start_city_index: the restaurant index
+    :param count_city: number of restaurant
+    :return:
+    """
     restaurantList = getRestaurant.get_restaurants_city(city_id, start_city_index, count_city)
 
     reviewList = []
