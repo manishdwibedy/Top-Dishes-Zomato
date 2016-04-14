2a2104afe401bbfeee99ed8d731767de56a2fbd4
Manish Dwibedy <manish.dwibedy@gmail.com>
2016-04-14 16:23:46 -0700

    Getting an unannotated review words from solr.

---
+++ Get_Zomato_Solr/getUnannotatedReviews.py
@@ -0,0 +1,18 @@
+from solr import connection, query
+from util import constant
+
+def getUntaggedReviews():
+    conn = connection.get_connection()
+    data = query.get(conn, constant.REVIEWS_COLLECTION, 'annotated:false', 1)
+
+    reviewsList = data.result.response.docs
+
+    if len(reviewsList) == 1:
+        return reviewsList[0]['review_text'][0]
+    else:
+        return None
+
+
+if __name__ == '__main__':
+    reviews = getUntaggedReviews()
+    pass
