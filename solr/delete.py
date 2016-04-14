b3cd4c8b8fbc0d7c30aac42c80ddbfb2f536935a
Manish Dwibedy <manish.dwibedy@gmail.com>
2016-04-14 09:54:34 -0700

    Now deleting data needs one more parameter - collection_name

--- solr/delete.py
+++ solr/delete.py
@@ -1,15 +1,22 @@
 import connection
 import query
 
-def delete(connection, query):
-    connection["zomato"].delete({'q':query})
-    connection["zomato"].commit()
+def delete(connection, collection, query):
+    """
+    Deleting the documents from the collection matching the query
+    :param connection: the Solr Connection
+    :param collection: the Solr Collection
+    :param query: Query
+    :return: Nothing
+    """
+    connection[collection].delete({'q':query})
+    connection[collection].commit()
 
 if __name__ == '__main__':
     connection = connection.get_connection()
-    queryString = 'id:*'
-    delete(connection, queryString)
+    queryString = 'id:2'
+    delete(connection, 'zomato_reviews',queryString)
 
-    output = query.get(connection, '*:*')
-    print output
+    # output = query.get(connection, '*:*')
+    # print output
 
