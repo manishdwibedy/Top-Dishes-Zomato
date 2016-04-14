12ae323fe394a15006149c127865f3ae85dc87a8
Manish Dwibedy <manish.dwibedy@gmail.com>
2016-04-14 10:22:41 -0700

    Now, loading restaurant's info in the solr collection.

--- Load_Zomato_Solr/loadRestaurant.py
+++ Load_Zomato_Solr/loadRestaurant.py
@@ -1,9 +1,10 @@
 from Zomato import getRestaurant
 from util import constant
+from solr import connection, index
 
 def getRestaurants(city_id, city_index, city_count):
     """
-    Getting the reviews for 'city_count' number of restaurants in the city referred by city_id.
+    Getting the restaurant info for 'city_count' number of restaurants in the city referred by city_id.
     The first restaurant's index would be city_index
 
     :param city_id: the city's ID
@@ -15,7 +16,20 @@ def getRestaurants(city_id, city_index, city_count):
 
     return restaurants
 
+def addToSolr(restaurants):
+    """
+    Add the restaurants to the solr index
+    :param restaurants: the restaurants to be index
+    :return: Nothing
+    """
+    conn = connection.get_connection()
+    index.index(conn, constant.RESTAURANTS_COLLECTION, restaurants)
+    pass
+
+
 if __name__ == '__main__':
     New_Delhi = constant.city_id
     restaurants = getRestaurants(New_Delhi, 0, 2)
+
+    addToSolr(restaurants)
     pass
