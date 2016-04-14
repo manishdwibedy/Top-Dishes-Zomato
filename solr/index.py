2af349edd4d639212742530c5fa0894680ebe2da
Manish Dwibedy <manish.dwibedy@gmail.com>
2016-04-14 09:49:43 -0700

    Now indexing data needs one more parameter - collection_name

--- solr/index.py
+++ solr/index.py
@@ -16,7 +16,7 @@ if __name__ == '__main__':
     connection = connection.get_connection()
     docs = [{"id":"1", "name":"a"},{"id":"2","name":"b"}]
 
-    index(connection, docs)
-    op = query.get(connection, '*:*')
-    print op
+    index(connection, 'zomato_reviews',docs)
+    # op = query.get(connection, '*:*')
+    # print op
 
