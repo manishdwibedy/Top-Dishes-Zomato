8e9bf17e105fa3de3bbfa3ce1f716afbaef80232
dhruvbha <dhruvbha@usc.edu>
2016-04-16 19:02:57 -0700

    Implemented Fuzzy Match

--- Matching/match.py
+++ Matching/match.py
@@ -1,5 +1,5 @@
-# from fuzzywuzzy import fuzz
-# from Levenshtein import *
+import math
+
 
 def exactMatch(reviewFoodItem, menuItem):
     """
@@ -30,9 +30,17 @@ def partialMatch(reviewFoodItem, menuItem):
     possibleMatches=[]
     for item in menuItem:
 
-        if numWordsExactMatches(reviewFoodItem,item) >= length / 2:
+        if numWordsExactMatches(reviewFoodItem,item) >= math.ceil(float(length) / 2):
             possibleMatches.append(item)
     print possibleMatches
+
+def fuzzyMatch(reviewFoodItem, menuItem):
+    possibleMatches=[]
+    for item in menuItem:
+        if numWordsMatch(reviewFoodItem,item):
+            possibleMatches.append(item)
+    return '' . join(possibleMatches)
+
 
 def numWordsExactMatches(mention, item):
     words = mention.split(" ")
@@ -44,80 +52,36 @@ def numWordsExactMatches(mention, item):
                 numMatches =numMatches+1
     return numMatches
 
-# def numWordsMatch(mention, item):
-#     mentionWords = mention.split(" ")
-#     itemWords = item.split(" ")
-#     numMatches = 0
-#     for i in len(mentionWords):
-#         for j in len(itemWords):
-#             if len(itemWords[j].strip) < 1:
-#                 continue
-
-# def damerau_levenshtein_distance(string1, string2):
-#     distance = {}
-#     lenstr1 = len(string1)
-#     lenstr2 = len(string2)
-#     for i in xrange(-1, lenstr1 + 1):
-#         distance[(i, -1)] = i + 1
-#     for j in xrange(-1, lenstr2 + 1):
-#         distance[(-1, j)] = j + 1
-#
-#     for i in xrange(lenstr1):
-#         for j in xrange(lenstr2):
-#             if string1[i] == string2[j]:
-#                 cost = 0
-#             else:
-#                 cost = 1
-#             distance[(i, j)] = min(
-#                             distance[(i - 1, j)] + 1,  # deletion
-#                             distance[(i, j - 1)] + 1,  # insertion
-#                             distance[(i - 1, j - 1)] + cost,  # substitution
-#                         )
-#             if i and j and string1[i] == string2[j - 1] and string1[i - 1] == string2[j]:
-#                 distance[(i, j)] = min(distance[(i, j)], distance[i - 2, j - 2] + cost)  # transposition
-#
-#
-#
-#
-#     return distance[lenstr1 - 1, lenstr2 - 1]
+def numWordsMatch(mentionWords, itemWords):
+    distance ={}
+    for i in xrange(-1, len(mentionWords) + 1):
+        distance[(i, -1)] = i + 1
+    for j in xrange(-1, len(itemWords) + 1):
+        distance[(-1, j)] = j + 1
+    for i in xrange(len(mentionWords)):
+        for j in xrange(len(itemWords)):
+            if mentionWords[i] == itemWords[j]:
+                cost = 0
+            else:
+                cost = 1
+            distance[(i, j)] = min(
+                                distance[(i - 1, j)] + 1,
+                                distance[(i, j - 1)] + 1,
+                                distance[(i - 1, j - 1)] + cost,
+                            )
+            if i and j and mentionWords[i] == itemWords[j - 1] and mentionWords[i - 1] == itemWords[j]:
+                distance[(i, j)] = min(distance[(i, j)], distance[i - 2, j - 2] + cost)
+    distance = distance[len(mentionWords) - 1, len(itemWords) - 1]
+    if distance < 0.5 * len(mentionWords):
+        return True
+    else:
+        return False
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-# public static int numWordsMatch(Mention m, MenuItem item) {
-#     String[] mentionWords = clean(m.mentionText).split(" ");
-#     String[] itemWords = clean(item.name + " " + item.description).split(" ");
-#
-#     DamerauLevenshtein d;
-#     int numMatches = 0;
-#     for (int i = 0; i < mentionWords.length; i++) {
-#       for (int j = 0; j < itemWords.length; j++) {
-#         if (trim(itemWords[j]).length() < 1) continue;
-#
-#         try {
-#           d = new DamerauLevenshtein(mentionWords[i], itemWords[j]);
-#           if (d.getDHSimilarity() <= DAMERAU_LEVENSHTEIN_MAX) {
-#             numMatches++;
-#             break;
-#           }
-#         } catch (Exception e) { }
-#       }
-#     }
-#     return numMatches;
 
 # reviewFoodItem="Chicken Tikka Masala"
 # menuItem=['Butter Chicken','Chicken Tikka Masala', 'Tikka Chicken Masala', 'Chicken Tika Masala', 'Chicken Tikka Tandori','Paneer Tikka Masala','Mango Lassi','Tandori','Tikka']
 # partialMatch(reviewFoodItem,menuItem)
 # exactMatch(reviewFoodItem,menuItem)
 # # fuzz.ratio("this is a test", "this is a test!")
-print damerau_levenshtein_distance("this is a test", "Dhruv")
+print fuzzyMatch("chicken tipa", ["Chicken Tikka", "Panner Tikka"])
