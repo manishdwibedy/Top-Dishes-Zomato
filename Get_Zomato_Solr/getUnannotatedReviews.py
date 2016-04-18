5608c5e806b6d7404df37b5596972cdac4fe54f9
Manish Dwibedy <manish.dwibedy@gmail.com>
2016-04-17 18:43:48 -0700

    Created a method to split a word if the word has a punctuation.

--- Get_Zomato_Solr/getUnannotatedReviews.py
+++ Get_Zomato_Solr/getUnannotatedReviews.py
@@ -2,8 +2,28 @@ from solr import connection, query
 from util import constant
 import re
 
-def seperatePunctuations():
-    pass
+# The characters that would be seperated
+chars = set('-!$%^&*()_+|~=`{}\[\]:;<>?,.\/')
+
+def seperatePunctuations(word):
+    """
+    Seperating the punctuations
+    :param word: the word, where we would search for any punctuations
+    :return: a list of seperated words, if any. Otherwise single element list is returned
+    """
+    seperatedWords = []
+    if any((c in chars) for c in word):
+        insertedSpace = re.sub(r'(.*)([-!$%^&*()_+|~=`{}\[\]:";<>?,.\/])(.*)', r"\1 \2 \3",  word).strip()
+        spaces = insertedSpace.split(' ')
+        for spaceText in spaces:
+            seperatedWords.append(spaceText)
+    elif word.endswith(u'\u0964'):
+        seperatedWords.append(word[:-1])
+        seperatedWords.append(u'\u0964')
+    else:
+        seperatedWords.append(word)
+    return seperatedWords
+
 def getUntaggedReviews():
     conn = connection.get_connection()
     data = query.get(conn, constant.REVIEWS_COLLECTION, 'annotated:false', 1)
@@ -15,17 +35,9 @@ def getUntaggedReviews():
         words = hindi[2:-1].split(' ')
         reviewText = []
         for word in words:
-            chars = set('-!$%^&*()_+|~=`{}\[\]:;<>?,.\/')
-            if any((c in chars) for c in word):
-                insertedSpace = re.sub(r'(.*)([-!$%^&*()_+|~=`{}\[\]:";<>?,.\/])(.*)', r"\1 \2 \3",  word).strip()
-                spaces = insertedSpace.split(' ')
-                for spaceText in spaces:
-                    reviewText.append(spaceText)
-            elif word.endswith(u'\u0964'):
-                reviewText.append(word[:-1])
-                reviewText.append(u'\u0964')
-            else:
-                reviewText.append(word)
+            without_seperatePunctuations = seperatePunctuations(word)
+            for word_w in without_seperatePunctuations:
+                reviewText.append(word_w)
         return {
             'id': reviewsList[0]['id'],
             'res_id': reviewsList[0]['res_id'],
