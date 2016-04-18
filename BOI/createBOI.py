4450d404e9df388f766f5ce967f86bc352dfb6f7
Manish Dwibedy <manish.dwibedy@gmail.com>
2016-04-17 20:35:20 -0700

    BOI tagging of the food items has been completed to be used by NER algorithm.

--- BOI/createBOI.py
+++ BOI/createBOI.py
@@ -15,32 +15,64 @@ def getReviews():
     reviews = response.result.dict['response']['docs']
     return reviews
 
+def createFoodDict(food_item_list):
+    """
+    Create a dictionary where the first word is key and the complete word is the value
+    :param food_item_list: the list of food items
+    :return: the dictionary of food items
+    """
+    food_dict = {}
+    for food_item in food_item_list:
+        words = food_item.split(' ')
+        first_word = words[0]
+        food_dict[first_word] = food_item
+    return food_dict
+
+
 def createBOI():
     """
     Creating the BOI annotations
-    :return: Nothing
+    :return: list of reviews with the BOI tags
     """
     reviews = getReviews()
+
+    BOI_reviews = []
 
     for review in reviews:
         food_item_list = review['food_item']
         hindi_review = review['hindi'][0]
 
 
-        food_items = dict((food_item,1) for food_item in food_item_list)
+        # food_items = dict((food_item,1) for food_item in food_item_list)
+        food_items = createFoodDict(food_item_list)
         review_words = hindi_review.split(' ')
 
         BOI_list = []
-        for review_word in hindi_review.split(' '):
+
+        review_words_enumerator = enumerate(review_words)
+        for index, review_word in review_words_enumerator:
             seperated_word_list = getUnannotatedReviews.seperatePunctuations(review_word)
 
             for word in seperated_word_list:
                 if word == '':
                     continue
                 if word in food_items:
-                    BOI_list.append(word+'/B')
+                    menu_length = len(food_items[word].split(' '))
+                    if menu_length > 1:
+                        BOI_list.append(word + '/B')
+                        for counter in range(index+1, index+menu_length):
+                            next_word = review_words_enumerator.next()
+                            BOI_list.append(next_word[1] + '/I')
+                            index +=1
+                    else:
+                        BOI_list.append(word+'/B')
                 else:
                     BOI_list.append(word+'/O')
+        BOI_reviews.append(' '.join(BOI_list))
+
+    return BOI_reviews
 
 if __name__ == '__main__':
-    createBOI()
+    BOI_list = createBOI()
+
+    print BOI_list
