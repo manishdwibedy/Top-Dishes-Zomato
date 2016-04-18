112600583e570085325b3a6e3ef7eda6bd1b2ada
dhruvbha <dhruvbha@usc.edu>
2016-04-17 21:38:53 -0700

    Extracting the menu item and its sentiment from the solr instance.

--- console_ui/readInput.py
+++ console_ui/readInput.py
@@ -4,6 +4,18 @@ def readInput():
     text = raw_input().decode(sys.stdin.encoding or locale.getpreferredencoding(True))
     return text.split(' ')
 
+def getMatchingText(input):
+    reviews = []
+    for review in reviews:
+        food_mentions = []
+        for index, menu in enumerate(review.menu_items):
+            food_mention = {
+                'menu_item': menu,
+                'sentiment': review.sentiment[index]
+            }
+            food_mentions.append(food_mention)
+            pass
+
 if __name__ == '__main__':
     words = readInput()
     print words
