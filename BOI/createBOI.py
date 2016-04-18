be0e846f69d8ff08fe7f3b54700a06b6dbecd3a3
Manish Dwibedy <manish.dwibedy@gmail.com>
2016-04-17 19:04:29 -0700

    Now, I can assign 'B' and 'O' tag to the review text. But need to work on the adding the 'I' tag.

--- BOI/createBOI.py
+++ BOI/createBOI.py
@@ -1,6 +1,6 @@
 from solr import connection, query
 from util import constant
-
+from Get_Zomato_Solr import getUnannotatedReviews
 
 def getReviews():
     """
@@ -15,5 +15,32 @@ def getReviews():
     reviews = response.result.dict['response']['docs']
     return reviews
 
+def createBOI():
+    """
+    Creating the BOI annotations
+    :return: Nothing
+    """
+    reviews = getReviews()
+
+    for review in reviews:
+        food_item_list = review['food_item']
+        hindi_review = review['hindi'][0]
+
+
+        food_items = dict((food_item,1) for food_item in food_item_list)
+        review_words = hindi_review.split(' ')
+
+        BOI_list = []
+        for review_word in hindi_review.split(' '):
+            seperated_word_list = getUnannotatedReviews.seperatePunctuations(review_word)
+
+            for word in seperated_word_list:
+                if word == '':
+                    continue
+                if word in food_items:
+                    BOI_list.append(word+'/B')
+                else:
+                    BOI_list.append(word+'/O')
+
 if __name__ == '__main__':
-    getReviews()
+    createBOI()
